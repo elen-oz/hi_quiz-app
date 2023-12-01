b61522865fcbd3d16201df2fd1f68ebec9f31723
osman kelifa <o_kelifa@hotmail.com>
2023-12-01 15:17:51 +0100

    project started

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hi');
+"use strict";
+let questions;
+let contanerEl = document.querySelector("#contaner");
+
+async function getQuestion() {
+  let url = "https://opentdb.com/api.php?amount=10&type=multiple";
+  let response = await fetch(url);
+  let data = await response.json();
+
+  questions = data;
+  console.log(questions);
+  let questionEL = document.createElement("div");
+
+  questionEL.innerHTML = questions;
+}
+
+getQuestion();
