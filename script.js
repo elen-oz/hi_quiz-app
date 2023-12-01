ae6a11842a6a2ff14c5e30d544576a97cc1b966f
elen-oz <ozerova.lenka@gmail.com>
2023-12-01 16:15:20 +0100

    add counter for right answers

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-"use strict";
+'use strict';
+
 let questions;
-let contanerEl = document.querySelector("#contaner");
+let currentQuestion = 0;
+let rightAnswers = 0;
+
+let containerEl = document.querySelector('#container');
 
 async function getQuestion() {
-  let url = "https://opentdb.com/api.php?amount=10&type=multiple";
+  let url = 'https://opentdb.com/api.php?amount=10&type=multiple';
   let response = await fetch(url);
   let data = await response.json();
 
-  questions = data;
-  console.log(questions);
-  let questionEL = document.createElement("div");
+  questions = data.results;
 
-  questionEL.innerHTML = questions;
+  renderQuestion(questions[currentQuestion]);
+}
+
+function renderQuestion(question) {
+  containerEl.innerHTML = '';
+  let correctAnswer = question.correct_answer;
+
+  let questionEl = document.createElement('div');
+  questionEl.innerHTML = question.question;
+  containerEl.append(questionEl);
+
+  console.log('correct answer', correctAnswer);
+
+  let answers = question.incorrect_answers.concat([question.correct_answer]);
+  answers = answers.sort(() => 0.5 - Math.random());
+
+  for (let i = 0; i < answers.length; i++) {
+    let answer = answers[i];
+    let answerEl = document.createElement('button');
+    answerEl.innerHTML = answer;
+
+    answerEl.addEventListener('click', function () {
+      let currentAnswer = answerEl.innerHTML;
+      if (currentAnswer === correctAnswer) {
+        rightAnswers += 1;
+        console.log('Right Answers: ' + rightAnswers);
+        console.log('Correct!');
+      } else {
+        console.log('Not correct');
+      }
+
+      if (currentQuestion < questions.length - 1) {
+        currentQuestion += 1;
+        renderQuestion(questions[currentQuestion]);
+      } else {
+        console.log('Quiz completed. Total right answers: ' + rightAnswers);
+      }
+    });
+
+    containerEl.append(answerEl);
+  }
 }
 
 getQuestion();
