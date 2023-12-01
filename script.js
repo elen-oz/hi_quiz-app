12188b7cf1f0528086b56e939a1bc555d52095cc
osman kelifa <o_kelifa@hotmail.com>
2023-12-01 17:37:26 +0100

    changes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-'use strict';
+"use strict";
 
 let questions;
 let currentQuestion = 0;
 let rightAnswers = 0;
 
-let containerEl = document.querySelector('#container');
+let containerEl = document.querySelector("#container");
+containerEl.classList.add("container");
+let buttonContainerEl = document.createElement("div");
+buttonContainerEl.classList.add("button-container");
+containerEl.append(buttonContainerEl);
 
 async function getQuestion() {
-  let url = 'https://opentdb.com/api.php?amount=10&type=multiple';
+  let url = "https://opentdb.com/api.php?amount=10&type=multiple";
   let response = await fetch(url);
   let data = await response.json();
 
@@ -18,46 +22,48 @@ async function getQuestion() {
 }
 
 function renderQuestion(question) {
-  containerEl.innerHTML = '';
-  let numberOfQuestions = document.createElement('h2');
+  // Clear the content of button-container
+  buttonContainerEl.innerHTML = "";
+
+  let numberOfQuestions = document.createElement("h2");
   numberOfQuestions.textContent = `${currentQuestion + 1} / 10`;
   containerEl.append(numberOfQuestions);
 
   let correctAnswer = question.correct_answer;
 
-  let questionEl = document.createElement('div');
+  let questionEl = document.createElement("div");
   questionEl.innerHTML = question.question;
   containerEl.append(questionEl);
 
-  console.log('correct answer', correctAnswer);
+  console.log("correct answer", correctAnswer);
 
   let answers = question.incorrect_answers.concat([question.correct_answer]);
   answers = answers.sort(() => 0.5 - Math.random());
 
   for (let i = 0; i < answers.length; i++) {
     let answer = answers[i];
-    let answerEl = document.createElement('button');
+    let answerEl = document.createElement("button");
     answerEl.innerHTML = answer;
-
-    answerEl.addEventListener('click', function () {
+    answerEl.classList.add("button");
+    answerEl.addEventListener("click", function () {
       let currentAnswer = answerEl.innerHTML;
       if (currentAnswer === correctAnswer) {
         rightAnswers += 1;
-        console.log('Right Answers: ' + rightAnswers);
-        console.log('Correct!');
+        console.log("Right Answers: " + rightAnswers);
+        console.log("Correct!");
       } else {
-        console.log('Not correct');
+        console.log("Not correct");
       }
 
       if (currentQuestion < questions.length - 1) {
         currentQuestion += 1;
         renderQuestion(questions[currentQuestion]);
       } else {
-        console.log('Quiz completed. Total right answers: ' + rightAnswers);
+        console.log("Quiz completed. Total right answers: " + rightAnswers);
       }
     });
 
-    containerEl.append(answerEl);
+    buttonContainerEl.append(answerEl);
   }
 }
 
