7de296678ae85211cdfec1d0ac0db262a4680af7
elen-oz <ozerova.lenka@gmail.com>
2023-12-04 16:30:51 +0100

    feat: add commits to each block of code

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 'use strict';
 
+// Declare variables to store quiz data and state.
 let questions;
 let questionsNumber = 10;
 let currentQuestion = 0;
 let rightAnswers = 0;
 
+// Find the HTML element with the ID 'app' to use it in the script.
 let appEl = document.querySelector('#app');
 
+// Create a new 'div' element for the container and add it to the 'app' element.
 let containerEl = document.createElement('div');
 containerEl.setAttribute('id', 'container');
 containerEl.classList.add('container');
 appEl.append(containerEl);
 
+// Create a button to start the quiz again and add it to the container.
 let startAgainBtnEl = document.createElement('button');
 startAgainBtnEl.classList.add('button', 'button--again');
 startAgainBtnEl.textContent = 'Start';
 containerEl.prepend(startAgainBtnEl);
 
+// Create elements to display the number of questions
 let questionsNumberEl = document.createElement('h2');
 questionsNumberEl.classList.add('questions-number');
+
+// Create elements to display the question itself
 let questionEl = document.createElement('div');
 questionEl.classList.add('question');
 containerEl.append(questionEl);
+
+// Create elements to display container where buttons for answers will be appear
 let buttonContainerEl = document.createElement('div');
 buttonContainerEl.classList.add('button-container');
 containerEl.append(buttonContainerEl);
+
+// Create elements to display the result.
 let resultEl = document.createElement('div');
 resultEl.classList.add('result');
 containerEl.append(resultEl);
 
+// questions from an API and display the first question.
 async function getQuestion() {
   let url = `https://opentdb.com/api.php?amount=${questionsNumber}&category=9&type=multiple`;
   let response = await fetch(url);
@@ -40,6 +52,7 @@ async function getQuestion() {
   renderQuestion(questions[currentQuestion]);
 }
 
+// displays a question and its answers.
 function renderQuestion(question) {
   buttonContainerEl.innerHTML = '';
   questionsNumberEl.innerHTML = '';
@@ -57,6 +70,7 @@ function renderQuestion(question) {
   let answers = question.incorrect_answers.concat([question.correct_answer]);
   answers = answers.sort(() => 0.5 - Math.random());
 
+  // Create buttons for each answer and add a click event to check the answer.
   for (let i = 0; i < answers.length; i++) {
     let answer = answers[i];
     let answerEl = document.createElement('button');
@@ -64,6 +78,8 @@ function renderQuestion(question) {
     answerEl.classList.add('button');
     answerEl.addEventListener('click', function () {
       let currentAnswer = answerEl.innerHTML;
+
+      // Go to the next question or end the quiz if all questions are answered.
       if (currentAnswer === correctAnswer) {
         rightAnswers += 1;
         console.log('! Right Answers: ' + rightAnswers);
@@ -79,6 +95,8 @@ function renderQuestion(question) {
         resultEl.textContent = `Quiz completed. Total right answers: ${rightAnswers}`;
 
         questionsNumberEl.innerHTML = 'FINISH';
+
+        // remove unnecessary fields
         questionEl.innerHTML = '';
         buttonContainerEl.innerHTML = '';
       }
@@ -88,6 +106,7 @@ function renderQuestion(question) {
   }
 }
 
+// Add a click event to the 'start again' button to reset the quiz and fetch new questions.
 startAgainBtnEl.addEventListener('click', () => {
   currentQuestion = 0;
   rightAnswers = 0;
