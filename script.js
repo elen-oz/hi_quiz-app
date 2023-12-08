14708ae6a876670df1bff30f88dcf603eeee762c
elen-oz <ozerova.lenka@gmail.com>
2023-12-08 16:45:34 +0100

    Co-authored-by: osmankelifa <osmankelifa@users.noreply.github.com>

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ let currentQuestion = 0;
 let rightAnswers = 0;
 let contributors = ['Elena', 'Osman', 'Jordan'];
 
+// create "history" variable for storage data of results
+let history;
+
+// here we getting data from localStorage.
+let dataFromLocalStorage = localStorage.getItem('history');
+
+// checking if there any information inside localStorage
+if (dataFromLocalStorage) {
+  // if there is, then we need "parse" it:
+  history = JSON.parse(dataFromLocalStorage);
+} else {
+  // if there isn't, then just give empty array to this variable
+  history = [];
+}
+
 // Find the HTML element with the ID 'app' to use it in the script.
 let appEl = document.querySelector('#app');
 
@@ -15,6 +30,16 @@ let containerEl = document.createElement('div');
 // containerEl.setAttribute('id', 'container');
 containerEl.classList.add('container');
 appEl.append(containerEl);
+
+// create a new 'div' for history
+let historyEl = document.createElement('div');
+// add class to the element
+historyEl.classList.add('history');
+// historyEl.textContent = 'Previous Results:';
+containerEl.append(historyEl);
+
+// call a function, that displays previous results
+displayHistory();
 
 // footer
 let footerEl = document.createElement('footer');
@@ -61,6 +86,8 @@ async function getQuestion() {
 
 // displays a question and its answers.
 function renderQuestion(question) {
+  // adding our history element to container of the game
+  containerEl.append(historyEl);
   buttonContainerEl.innerHTML = '';
   questionsNumberEl.innerHTML = '';
 
@@ -99,6 +126,13 @@ function renderQuestion(question) {
         currentQuestion += 1;
         renderQuestion(questions[currentQuestion]);
       } else {
+        // add to the end of "history" array new result
+        history[history.length] = rightAnswers;
+
+        // add history to localStorage
+        localStorage.setItem('history', JSON.stringify(history));
+        displayHistory();
+
         resultEl.textContent = `Quiz completed. Total right answers: ${rightAnswers}`;
 
         questionsNumberEl.innerHTML = 'FINISH';
@@ -113,6 +147,29 @@ function renderQuestion(question) {
   }
 }
 
+// this function display history
+function displayHistory() {
+  // 1) first we need to check if there was any result or it's first game
+  // if length of array is 0, it's mean that there wasn't game before (localStorage is empty) so we display just line: 'Let's Start'
+  if (history.length === 0) {
+    historyEl.innerHTML = `Let's Start!`;
+  } else {
+    // 2) otherwise there were some results and we need to display them instead
+    // we add litle title for the list:
+    historyEl.innerHTML = 'Previous Results:';
+
+    // here is a loop, where we display each element of array one by one
+    for (let i = 0; i < history.length; i += 1) {
+      // create <div> element
+      let scoreEl = document.createElement('div');
+      // add each element as text inside the tag
+      scoreEl.innerHTML = history[i];
+      // add this element to parent element which is historyEl
+      historyEl.appendChild(scoreEl);
+    }
+  }
+}
+
 // Add a click event to the 'start again' button to reset the quiz and fetch new questions.
 startAgainBtnEl.addEventListener('click', () => {
   currentQuestion = 0;
@@ -121,3 +178,5 @@ startAgainBtnEl.addEventListener('click', () => {
 
   getQuestion();
 });
+
+// You can add 1 more button - to delete localStorage and clean results
